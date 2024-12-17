d62356aee7a45ca7f29fbe235fac9706bf6df7e8
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2024-12-17 16:02:29 +0300

    separated forms and v0.1.0

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,121 +1,31 @@
 import { useAuth } from "@/hooks/use-auth";
 import { useProfileData } from "@/hooks/use-profile-data";
-import { db } from "@/lib/db";
-import type { ProfileData } from "@/lib/schemas";
 import { JoinSessionButton } from "@/pages/dashboard/join-session-button";
-import { type FormEvent, useState } from "react";
-import { toast } from "sonner";
-import { useOrganization } from "./use-organization";
+import { ChangeNameForm } from "./change-name-form";
+import { ChangeOrganizationForm } from "./change-org-form";
 
 export function DashboardPage() {
-	console.count("🚀 Dashboard page rendered");
+  console.count("🚀 Dashboard page rendered");
 
-	const { user } = useAuth();
-	const { profileData, isPending } = useProfileData(user?.uid);
+  const { user } = useAuth();
+  const { profileData, isPending } = useProfileData(user?.uid);
 
-	if (isPending) {
-		return <p>Loading...</p>;
-	}
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
 
-	if (!profileData) {
-		return <p>Not logged in</p>;
-	}
+  if (!profileData) {
+    return <p>Not logged in</p>;
+  }
 
-	return (
-		<div className="container mx-auto space-y-6 px-16 py-8 lg:px-32">
-			<h1>Welcome to Refination!</h1>
-			<div className="grid grid-cols-1 gap-8 *:flex *:flex-col *:justify-between *:rounded-xl *:border *:p-8 sm:grid-cols-3">
-				<JoinSessionButton profileData={profileData} />
-				<ChangeOrganizationForm profileData={profileData} />
-				<ChangeNameForm profileData={profileData} />
-			</div>
-		</div>
-	);
-}
-
-function ChangeOrganizationForm({ profileData }: { profileData: ProfileData }) {
-	const [orgId, setOrgId] = useState(profileData.user.orgId);
-
-	const { organization } = useOrganization(orgId);
-
-	async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
-		const newOrgId = formData.get("organization") as string;
-
-		try {
-			await db.runTransaction(async (transaction) => {
-				const orgRef = db.ref("organizations", newOrgId);
-				const orgDoc = await transaction.get(orgRef);
-
-				if (!orgDoc.exists) {
-					throw new Error("Organization does not exist");
-				}
-
-				const orgData = orgDoc.data();
-
-				if (!orgData) {
-					throw new Error("Organization data is missing");
-				}
-
-				const userRef = db.ref("users", profileData.user.id);
-				transaction.update(userRef, { orgId: newOrgId });
-				setOrgId(newOrgId);
-			});
-		} catch (error) {
-			console.error(error);
-			toast.error("Failed to change organization");
-		}
-	}
-
-	return (
-		<div>
-			<h3>You can change your organization here.</h3>
-			<p className="pb-4 font-medium text-muted-foreground">
-				Current organization: {organization?.name}
-			</p>
-			<form onSubmit={handleSubmit}>
-				<fieldset>
-					<label htmlFor="organization">Enter New Organization ID</label>
-					<input type="text" id="organization" name="organization" value={profileData.user.orgId} />
-				</fieldset>
-
-				<button type="submit">Change Organization</button>
-			</form>
-		</div>
-	);
-}
-
-function ChangeNameForm({ profileData }: { profileData: ProfileData }) {
-	const [name, setName] = useState(profileData.user.name);
-
-	async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-		event.preventDefault();
-		const formData = new FormData(event.currentTarget);
-		const newName = formData.get("name") as string;
-
-		try {
-			await db.update("users", profileData.user.id, { name: newName });
-		} catch (error) {
-			console.error(error);
-			toast.error("Failed to change name");
-		} finally {
-			setName(newName);
-		}
-	}
-
-	return (
-		<div>
-			<h3>You can change your name here.</h3>
-			<p className="pb-4 font-medium text-muted-foreground">Current name: {name}</p>
-			<form onSubmit={handleSubmit}>
-				<fieldset>
-					<label htmlFor="name">New Name</label>
-					<input type="text" id="name" name="name" />
-				</fieldset>
-
-				<button type="submit">Change Name</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="container mx-auto space-y-6 px-16 py-8 lg:px-32">
+      <h1>Welcome to Refination!</h1>
+      <div className="grid grid-cols-1 gap-8 *:flex *:flex-col *:justify-between *:rounded-xl *:border *:p-8 sm:grid-cols-3">
+        <JoinSessionButton profileData={profileData} />
+        <ChangeOrganizationForm profileData={profileData} />
+        <ChangeNameForm profileData={profileData} />
+      </div>
+    </div>
+  );
 }
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,107 +1,75 @@
 import { BadgeCheckIcon, UsersIcon, ZapIcon } from "@/components/icons";
 import { useAuth } from "@/hooks/use-auth";
 import { cn } from "@/lib/utils";
-import { type ReactNode, useEffect, useState } from "react";
-import Confetti from "react-confetti";
 import { Link } from "react-router";
 
 export function LandingPage() {
-	return (
-		<>
-			<div className="container mx-auto px-4 py-16 text-center">
-				<h2 className="mb-6 font-extrabold text-4xl md:text-5xl ">
-					Refine Tasks, Vote in Real-Time
-				</h2>
-				<p className="mx-auto mb-8 max-w-2xl text-muted-foreground text-xl dark:text-gray-300">
-					Streamline your team's task refinement process with our intuitive, real-time voting app.
-					Make decisions faster, together.
-				</p>
+  const { user } = useAuth();
 
-				<div className="flex flex-col-reverse gap-8 py-16 md:flex-col md:gap-16 md:py-16">
-					<div className="grid grid-cols-1 gap-8 md:grid-cols-3">
-						<FeatureCard
-							icon={<BadgeCheckIcon className="aspect-square size-12 text-success" />}
-							title="Efficient Refinement"
-							description="Quickly break down and estimate tasks with your team in real-time."
-						/>
-						<FeatureCard
-							icon={<UsersIcon className="aspect-square size-12 text-info" />}
-							title="Team Collaboration"
-							description="Foster better communication and alignment within your team."
-						/>
-						<FeatureCard
-							icon={<ZapIcon className="aspect-square size-12 text-warning" />}
-							title="Instant Results"
-							description="See voting results immediately and make informed decisions faster."
-						/>
-					</div>
+  return (
+    <>
+      <div className="container mx-auto px-4 py-16 text-center">
+        <h2 className="mb-6 font-extrabold text-4xl md:text-5xl ">
+          Refine Tasks, Vote in Real-Time
+        </h2>
+        <p className="mx-auto mb-8 max-w-2xl text-muted-foreground text-xl dark:text-gray-300">
+          Streamline your team's task refinement process with our intuitive, real-time voting app.
+          Make decisions faster, together.
+        </p>
 
-					<div>
-						<GetStartedLink />
-					</div>
-				</div>
-			</div>
+        <div className="flex flex-col-reverse gap-8 py-16 md:flex-col md:gap-16 md:py-16">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+            <div className="rounded-2xl bg-gradient-to-t from-indigo-600/10 to-indigo-400/10 px-6 py-12 shadow-md">
+              <div className="mb-6 flex justify-center">
+                <BadgeCheckIcon className="aspect-square size-12 text-success" />
+              </div>
+              <h3 className="mb-3">Efficient Refinement</h3>
+              <p className="text-neutral-200">
+                Quickly break down and estimate tasks with your team in real-time.
+              </p>
+            </div>
+            <div className="rounded-2xl bg-gradient-to-t from-indigo-600/10 to-indigo-400/10 px-6 py-12 shadow-md">
+              <div className="mb-6 flex justify-center">
+                <UsersIcon className="aspect-square size-12 text-info" />
+              </div>
+              <h3 className="mb-3">Team Collaboration</h3>
+              <p className="text-neutral-200">
+                Foster better communication and alignment within your team.
+              </p>
+            </div>
+            <div className="rounded-2xl bg-gradient-to-t from-indigo-600/10 to-indigo-400/10 px-6 py-12 shadow-md">
+              <div className="mb-6 flex justify-center">
+                <ZapIcon className="aspect-square size-12 text-warning" />
+              </div>
+              <h3 className="mb-3">Instant Results</h3>
+              <p className="text-neutral-200">
+                See voting results immediately and make informed decisions faster.
+              </p>
+            </div>
+          </div>
 
-			<footer className="container mx-auto px-4 py-8 text-center text-muted-foreground">
-				<p>&copy; 2024 Tabula Refination. All rights reserved.</p>
-			</footer>
-		</>
-	);
-}
+          <div>
+            <Link
+              to={user ? "/dashboard" : "/auth"}
+              className={cn(
+                "button primary rounded-full px-8 py-4 font-semibold text-lg shadow-lg",
+                "hover:disco-animation"
+              )}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </div>
 
-function FeatureCard({
-	icon,
-	title,
-	description,
-}: {
-	icon: ReactNode;
-	title: string;
-	description: string;
-}) {
-	return (
-		<div className="rounded-2xl bg-gradient-to-t from-indigo-600/10 to-indigo-400/10 px-6 py-12 shadow-md transition-transform hover:scale-105">
-			<div className="mb-6 flex justify-center">{icon}</div>
-			<h3 className="mb-3">{title}</h3>
-			<p className="text-neutral-200">{description}</p>
-		</div>
-	);
-}
-
-function GetStartedLink() {
-	const { user } = useAuth();
-
-	const [showConfetti, setShowConfetti] = useState(false);
-
-	const [size, setSize] = useState({ width: 0, height: 0 });
-
-	useEffect(() => {
-		setSize({ width: window.innerWidth, height: window.innerHeight });
-	}, []);
-
-	return (
-		<div>
-			<Link
-				to={user ? "/dashboard" : "/auth"}
-				className={cn(
-					"button primary rounded-full px-8 py-4 font-semibold text-lg shadow-lg",
-					"hover:spin-fast",
-				)}
-				onMouseEnter={() => setShowConfetti(true)}
-				onMouseLeave={() => setShowConfetti(false)}
-			>
-				Get Started
-			</Link>
-			{showConfetti && (
-				<Confetti
-					numberOfPieces={800}
-					run={showConfetti}
-					initialVelocityY={20}
-					initialVelocityX={20}
-					recycle={true}
-					gravity={0.5}
-					{...size}
-				/>
-			)}
-		</div>
-	);
+      <footer className="container mx-auto px-4 py-8 text-center text-muted-foreground">
+        <p>
+          This is <strong>Refination</strong> by <a href="https://ozanarslan.vercel.app">Tabula</a>.
+        </p>
+        <p>
+          Designed and developed by <a href="https://ozanarslan.vercel.app">Ozan Arslan</a>.
+        </p>
+      </footer>
+    </>
+  );
 }
